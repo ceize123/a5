1972f48178c54b52b9cc3faddd41e560b64f910c
ytkuo <hahaeating1@gmail.com>
2022-07-06 21:58:01 -0400

    first commit

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import albumData from '../data/SearchResultsAlbum.json';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MusicDataService } from '../music-data.service';
 
 @Component({
   selector: 'app-album',
   templateUrl: './album.component.html',
   styleUrls: ['./album.component.css']
 })
-export class AlbumComponent implements OnInit {
-    album = albumData;
-    
-    constructor() { }
+export class AlbumComponent implements OnInit, OnDestroy {
+    // album = albumData;
+    album: any = {};
+    paramsSubscription: Subscription | undefined;
+    musicDataSubscription: Subscription | undefined;
+
+    constructor(private snackBar: MatSnackBar, private route: ActivatedRoute, private mds: MusicDataService) { }
 
     ngOnInit(): void {
+        this.paramsSubscription = this.route.params.subscribe((params: Params) => {
+            let idParam = params["id"];
+            this.musicDataSubscription = this.mds.getAlbumById(idParam).subscribe((data: any) => {
+                this.album = data;
+            });
+        })
     }
 
+    addToFavourites(trackID: any): void{
+        if (this.mds.addToFavourites(trackID)) {
+            this.snackBar.open("Adding to Favourites...", "Done", { duration: 1500 });
+        };
+    }
+
+    ngOnDestroy(): void { // clean up
+        this.musicDataSubscription?.unsubscribe();
+        this.paramsSubscription?.unsubscribe();
+    }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
+/********************************************************************************* 
+* WEB422 – Assignment 05 
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this 
+* assignment has been copied manually or electronically from any other source (including web sites) or 
+* distributed to other students. 
+* 
+* Name: __Yi-Tso Kuo______ Student ID: ___166577197____ Date: __Mar. 17, 2022_____ 
+* 
+********************************************************************************/
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web422-a4';
+    title = 'web422-a4';
+    searchString: string = "";
+
+    constructor( private router: Router ){}
+
+    handleSearch() {
+        this.router.navigate(["/search"], { queryParams: { q: this.searchString } });
+        this.searchString = ""; // set value to empty
+    }
 }
